de92e30dbf9500afbebb9376a148051b90ff1536
Maurício Antunes <mauricio.abreua@gmail.com>
2019-12-17 11:44:58 -0300

    Solve day 2 problem - part 2

--- day2/main.go
+++ day2/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"io/ioutil"
+	"os"
 	"strconv"
 	"strings"
 )
@@ -30,27 +31,25 @@ func toInt(input string) int {
 	return number
 }
 
-// IntCode process a sequence of numbers and return
+// IntCode process a program of numbers and return
 // the first position of the generated sequence
-func IntCode(sequence []int) int {
-	restoredProgram := restore(sequence)
-
+func IntCode(program []int) int {
 	pos := 0
 
 	for {
-		opCode := restoredProgram[pos]
+		opCode := program[pos]
 
-		inputA := restoredProgram[pos+1]
-		inputB := restoredProgram[pos+2]
-		outputPos := restoredProgram[pos+3]
+		inputA := program[pos+1]
+		inputB := program[pos+2]
+		outputPos := program[pos+3]
 
 		switch opCode {
 		case 1:
-			restoredProgram[outputPos] = restoredProgram[inputA] + restoredProgram[inputB]
+			program[outputPos] = program[inputA] + program[inputB]
 		case 2:
-			restoredProgram[outputPos] = restoredProgram[inputA] * restoredProgram[inputB]
+			program[outputPos] = program[inputA] * program[inputB]
 		case 99:
-			return restoredProgram[0]
+			return program[0]
 		default:
 			return 0
 		}
@@ -58,14 +57,26 @@ func IntCode(sequence []int) int {
 	}
 }
 
-func restore(sequence []int) []int {
+func restore(sequence []int, noun int, verb int) []int {
 	restoredProgram := make([]int, len(sequence))
 	copy(restoredProgram, sequence)
-	restoredProgram[1], restoredProgram[2] = 12, 2
+	restoredProgram[1], restoredProgram[2] = noun, verb
 	return restoredProgram
 }
 
 func main() {
 	sequence := readSequence("input.txt")
-	fmt.Printf("Intcode is: %d\n", IntCode(sequence))
+	program := restore(sequence, 12, 2)
+	fmt.Printf("Intcode part 1: %d\n", IntCode(program))
+
+	for noun := 0; noun < 100; noun++ {
+		for verb := 0; verb < 100; verb++ {
+			program = restore(sequence, noun, verb)
+			result := IntCode(program)
+			if result == 19690720 {
+				fmt.Printf("IntCode part 2: %d\n", 100*noun+verb)
+				os.Exit(0)
+			}
+		}
+	}
 }
